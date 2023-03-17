a7d9001b3ba07f4a8538c3d20dbdce6fabbf0006
LucasPerrotaRoriz <Lucaswebdeveloper@proton.me>
2023-03-17 11:38:00 -0300

    Adding dynamic data and working with expressions in JSX

---
+++ Section 3 - React Basics and Working with Components/5-Dynamic-Data-Working-Expressions-JSX/src/App.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
